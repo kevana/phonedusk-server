35b5709534f838f6f2d7ac6e545f5631ac3b4d9e
Kevan Ahlquist <ahlqu039@umn.edu>
2014-11-08 15:47:31 -0600

    Updated database URL.

--- phonedusk/settings.py
+++ phonedusk/settings.py
@@ -19,7 +19,7 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'  # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = os_env.get('DATABASE_URL', 'postgresql://localhost/example')
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
